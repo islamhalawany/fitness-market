5780ebe37b46efa998ca5f3a78f38f62b35308e9
Islam Ibrahim <islamibrahim008@gmail.com>
2021-03-29 13:16:46 +0400

    second msg

--- src/Layout.js
+++ src/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router-dom';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/
 
 
 const Layout = (props) => {
